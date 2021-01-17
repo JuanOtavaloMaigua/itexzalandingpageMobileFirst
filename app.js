7d8b12b3a1970c154c955d8bba44b77944cb2baf
JuanOtavaloMaigua <juanotavalomaigua@gmail.com>
2021-01-17 09:36:45 -0500

    homepage avance mobile first

---
+++ app.js
@@ -0,0 +1,51 @@
+//Codigo para el creacion del slider en mobile
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider_container_slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      dots[i].style.backgroundColor = "#1458AA";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "grid";
+  dots[slideIndex-1].style.backgroundColor = "#F50006";
+
+  dots[slideIndex-1].className += " active";
+} 
+
+
+
+var slideIndexCopy= 1
+var slidesCopy = document.getElementsByClassName("somos_varios_container_slider")
+
+function automateSlider(){
+    for(i=0; i< slidesCopy.length; i++){
+        slidesCopy[i].style.display = "none"
+    }
+    slidesCopy[slideIndexCopy].style.display = "grid"
+
+    slideIndexCopy++
+    if(slideIndexCopy == (slideIndexCopy.length)){ slideIndexCopy = 0}
+
+    setTimeout(automateSlider, 3000)  
+}
+
+automateSlider()
